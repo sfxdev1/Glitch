9a5e02e987fb14732b319abe71b0c510f713e164
sfxdev <sfx.coding@gmail.com>
2016-11-15 18:35:49 -0600

    Third attempt at getting jumping to work.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,13 +5,14 @@ public class MovementScript : MonoBehaviour
 {
     public float Speed = 1;
     public float JumpSpeed = 8.0F;
-    public float Gravity = 9F;
+    public float Gravity;
     public GameObject Cam;
     public float VerticalMomentum;
     readonly CharacterController _controller;
 
     public MovementScript()
     {
+        Gravity = 9;
         _controller = GetComponent<CharacterController>();
     }
 
@@ -21,13 +22,14 @@ public class MovementScript : MonoBehaviour
 
     void Update()
     {
+        transform.rotation = new Quaternion(transform.rotation.x, Cam.transform.rotation.y, transform.rotation.z,Cam.transform.rotation.w);
         CharacterController controller = GetComponent<CharacterController>();
         if (Input.GetKey(KeyCode.W))
         {
             controller.SimpleMove(transform.forward * Speed);
         }
         VelCalc();
-        VerticalMomentum = _controller.isGrounded ? Input.GetAxis("Jump") : 0f;
+        VerticalMomentum = Input.GetAxis("Jump");
         UpdateTransform();
     }
 
